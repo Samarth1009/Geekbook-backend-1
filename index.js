c09c8453d51d00be53b75eb5683c1d468517b557
SKD <sarthakd762@gmail.com>
2021-12-26 23:29:59 +0530

    first commit2

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
-const { MongoClient } = require("mongodb");
 const app = express();
 
 app.use(express.json());
 
 const uri =
-  "mongodb+srv://noobdev:coder2020@cluster0.lf5dh.mongodb.net/Authentication?retryWrites=true&w=majority";
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-});
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  client.close();
-});
+  "mongodb+srv://noobdev:coder2020@cluster0.lf5dh.mongodb.net/Geekbook?retryWrites=true&w=majority";
 
 app.listen(3000, () => {
   console.log("Server is running...");
