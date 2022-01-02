93f0a617ffe9c40fae7649bb67287cf3265a7e35
SKD <sarthakd762@gmail.com>
2022-01-02 23:43:52 +0530

    dotenv

--- authentication/auth.js
+++ authentication/auth.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const userSchema = require("./schema/userSchema.js");
-
+const userSchema = require("../schema/userSchema.js");
 
 router.post("/signup", async (req, res) => {
-    userSchema.create(req.body, (err, data) => {
-      if (err) {
-        res.status(500).send(err);
-      } else {
-        res.status(200).send(data);
-      }
-    });
+  userSchema.create(req.body, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
   });
+});
 
-module.exports = router ;
+module.exports = router;
--- index.js
+++ index.js
@@ -2,16 +2,14 @@ const express = require("express");
 const app = express();
 const router = express.Router();
 const connectDB = require("./db-connect/db.js");
-require('dotenv').config()
+require("dotenv").config();
 const authRouter = require("./authentication/auth.js");
-
 
 connectDB();
 
 app.use(express.json());
 app.use(authRouter);
 
-
-app.listen(PORT, () => {
-  console.log(`Server started on PORT ${PORT} `);
+app.listen(process.env.PORT, () => {
+  console.log(`Server started on PORT ${process.env.PORT} `);
 });
