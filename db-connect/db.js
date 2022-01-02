166f30146c5a0b605606443da167dda8131ce8fa
Shashwat Kushwaha <vasuk8324@gmail.com>
2022-01-02 23:12:47 +0530

    signin updated

--- db-connect/db.js
+++ db-connect/db.js
@@ -1,13 +1,12 @@
 const mongoose = require("mongoose");
 const colors = require("colors");
 require('dotenv').config()
+
 const connectDB = async () => {
   try {
-    // console.log(process.env.MONGO_URI)
     const conn = await mongoose.connect(String(process.env.MONGO_URI), {
       useUnifiedTopology: true,
       useNewUrlParser: true,
-      // useCreateIndex: true,
     });
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
