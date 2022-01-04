7781b1f25138288fc0301637c347ec3eb04c68e4
Shashwat Kushwaha <vasuk8324@gmail.com>
2022-01-04 22:34:43 +0530

    backend signin added

--- authentication/auth.js
+++ authentication/auth.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const User = require("../schema/userSchema.js");
 const router = express.Router();
-const userSchema = require("../schema/userSchema.js");
+// const userSchema = require("../schema/userSchema.js");
 
 router.post("/signup", async (req, res) => {
-  userSchema.create(req.body, (err, data) => {
+  const username = req.body.username;
+  const userExists = await User.findOne({username});
+  if(userExists){
+     res.send("User already exists");
+    //  throw new Error("User already exists");
+  }
+
+  User.create(req.body, (err, data) => {
     if (err) {
       res.status(500).send(err);
     } else {
@@ -11,5 +19,21 @@ router.post("/signup", async (req, res) => {
     }
   });
 });
+
+router.post("/signin", async(req, res) => {
+  const {username, password} = req.body;
+  const user = await User.findOne({username});
+  if(user && (await user.matchPassword(password))){
+    res.send("Successfully signed in");
+    console.log("Successfully signed in");
+  }
+  else{
+    res.send("Invalid Username or Password");
+    throw new Error("Invalid Username or Password");
+  }
+ 
+});
+
+
 
 module.exports = router;
